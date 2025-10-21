c1b9fa6d25c16ea8863da54b4650413e8c125d36
JeongWonYoung <nicednjsdud@naver.com>
2025-10-21 12:52:29 +0900

    26. 타입 좁히기

---
+++ typescript/src/type/narrowing.ts
@@ -0,0 +1,41 @@
+/**
+ * 타입 좁히기 (Type Narrowing)
+ * 조건문 등을 이용해 넓은타입에서 좁은타입으로
+ * 타입을 상황에 따라 좁히는 방법을 설명합니다.
+ */
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+// value => number : fixed
+// value => string : toLowerCase
+// value => Date : getFullYear
+// value => null : "value is null"
+// value => Person : name은 age살 입니다.
+
+function func(value: number | string | Date | null | Person){
+    value; // value는 number | string 타입
+
+    // value.toUpperCase; // 에러: number 타입에는 toUpperCase 메서드가 없음
+    // value.toFixed; // 에러: string 타입에는 toFixed 메서드가 없음
+    
+    if(typeof value == 'number'){
+      return console.log(value.toFixed(2)); // number 타입으로 좁혀짐
+    }
+    else if(typeof value == 'string'){
+      return console.log(value.toLowerCase()); // string 타입으로 좁혀짐
+    }
+    else if(value instanceof Date){
+      return console.log(value.getFullYear()); // Date 타입으로 좁혀짐
+    }
+    else if("age" in value! && "name" in value!){
+      return console.log(`${value.name}은 ${value.age}살 입니다.`); // Person 타입으로 좁혀짐
+    }
+    else {
+      return console.log("value is null");
+    }
+};
+
+
