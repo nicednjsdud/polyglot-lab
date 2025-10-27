234b8f6295a9b4e38669c83bfa5262691ea1955a
JeongWonYoung <nicednjsdud@naver.com>
2025-10-27 12:54:40 +0900

    Create mappedType_3.js

---
+++ typescript/dist/9. utility/mappedType_3.js
@@ -0,0 +1,12 @@
+/**
+ * Exclude<T, U>
+ * -> 제외하다, 추방하다.
+ * -> T에서 U에 할당 가능한 타입들을 제외한 나머지 타입들로 이루어진 새로운 타입을 만들어주는 타입
+ */
+function funcA() {
+    return "hello";
+}
+function funcB() {
+    return 10;
+}
+export {};
